f078b945cc1243122cefd36df3cfc9e997200420
Samuel Kurian <samuelkurian@wifi-172-23-59-55.uoa-wifi.auckland.ac.nz>
2023-03-15 01:51:39 +1300

    Created User class

---
+++ src/main/java/nz/ac/auckland/se281/User.java
@@ -0,0 +1,11 @@
+package nz.ac.auckland.se281;
+
+public class User {
+    private String userName;
+    public Integer age;
+
+    public User(String userName, Integer age){
+        this.userName = userName;
+        this.age = age;
+    }
+}
