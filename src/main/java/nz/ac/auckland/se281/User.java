40e1ef2fe858a578a270d5985d3e383153c270c9
Samuel Kurian <samuelrkurian@gmail.com>
2023-03-19 22:16:57 +1300

    making fields private; making public getter methods to access these fields

--- src/main/java/nz/ac/auckland/se281/User.java
+++ src/main/java/nz/ac/auckland/se281/User.java
@@ -1,11 +1,19 @@
 package nz.ac.auckland.se281;
 
 public class User {
-    public String userName;
-    public Integer age;
+    private String userName;
+    private int age;
 
     public User(String userName, Integer age){
         this.userName = userName;
         this.age = age;
+    }
+
+    public String getUserName(){
+        return this.userName;
+    }
+
+    public Integer getAge(){
+        return this.age;
     }
 }
