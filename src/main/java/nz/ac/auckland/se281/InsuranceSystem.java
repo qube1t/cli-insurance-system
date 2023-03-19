a3672a61dffe0dc2044b6e4760bbc50e6c424adf
Samuel Kurian <samuelrkurian@gmail.com>
2023-03-19 22:17:20 +1300

    validation before creating profile

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -41,10 +41,10 @@ public class InsuranceSystem {
       // prints the details from user object
       // if the user is the loaded user, print asterisks
       if (this.loadedUser == user){
-        MessageCli.PRINT_DB_PROFILE_HEADER_SHORT.printMessage("*** ", i.toString(), userName, user.age.toString());
+        MessageCli.PRINT_DB_PROFILE_HEADER_SHORT.printMessage("*** ", i.toString(), userName, user.getAge().toString());
       }
       else {
-      MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(i.toString(), userName, user.age.toString());
+      MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(i.toString(), userName, user.getAge().toString());
       }
     }
 
@@ -52,24 +52,41 @@ public class InsuranceSystem {
   
   public void createNewProfile(String userName, String age) {
 
-    String formattedUserName = titleCase(userName);
+
+    int ageInt = Integer.parseInt(age);
+    String formattedUserName = makeTitleCase(userName);
+
     // validation
-    if (formattedUserName.length() < 3) {
+    // see if a user is already loaded
+    if (this.loadedUser != null){
+      MessageCli.CANNOT_CREATE_WHILE_LOADED.printMessage(loadedUser.getUserName());
+      return;
+    } 
+    // see if the username is too short
+    else if (formattedUserName.length() < 3) {
       MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(formattedUserName);
       return;
-    } else if (users.get(formattedUserName) != null){
+    } 
+    // see if the age is invalid
+    else if (ageInt < 0 || ageInt > 120){
+      MessageCli.INVALID_AGE.printMessage(age);
+      return;
+    } 
+    // see if the username is already taken
+    else if (users.get(formattedUserName) != null){
       MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(formattedUserName);
       return;
     }
     
-    User user = new User(formattedUserName, Integer.parseInt(age));
+    // creating new user and adding to hash
+    User user = new User(formattedUserName, ageInt);
     users.put(formattedUserName, user);
     this.numberOfProfiles++;
-    System.out.println("New profile created for " + formattedUserName + " with age " + age + ".");
+    System.out.println("New profile created for " + formattedUserName + " with age " + ageInt + ".");
     
   }
 
-  private String titleCase(String input) {
+  private String makeTitleCase(String input) {
     // for formatting userName
     String formattedString = input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
     return formattedString;
@@ -77,7 +94,7 @@ public class InsuranceSystem {
 
   public void loadProfile(String userName) {
     // setting the loaded user to be the inputed user
-    String formattedUserName = titleCase(userName);
+    String formattedUserName = makeTitleCase(userName);
 
     this.loadedUser = (User) users.get(formattedUserName);
 
@@ -98,14 +115,14 @@ public class InsuranceSystem {
     }
 
     // unloading the user
-    String userName = this.loadedUser.userName;
+    String userName = this.loadedUser.getUserName();
     this.loadedUser = null;
     MessageCli.PROFILE_UNLOADED.printMessage(userName);
   }
 
   public void deleteProfile(String userName) {
     // check if user exists
-    String formattedUserName = titleCase(userName);
+    String formattedUserName = makeTitleCase(userName);
     if (users.get(formattedUserName) == null) {
       MessageCli.NO_PROFILE_FOUND_TO_DELETE.printMessage(formattedUserName);
       return;
