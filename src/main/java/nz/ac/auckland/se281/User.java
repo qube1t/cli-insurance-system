1152da53e2f038760457dff102cffa64933c320e
Samuel Kurian <samuelrkurian@gmail.com>
2023-03-21 13:22:31 +1300

    formatting document

--- src/main/java/nz/ac/auckland/se281/User.java
+++ src/main/java/nz/ac/auckland/se281/User.java
@@ -4,16 +4,16 @@ public class User {
     private String userName;
     private int age;
 
-    public User(String userName, Integer age){
+    public User(String userName, Integer age) {
         this.userName = userName;
         this.age = age;
     }
 
-    public String getUserName(){
+    public String getUserName() {
         return this.userName;
     }
 
-    public Integer getAge(){
+    public Integer getAge() {
         return this.age;
     }
 }
