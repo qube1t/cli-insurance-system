bd7ff819c951ca1b3673f2821be1358e48ce5091
Samuel Kurian <samuelrkurian@gmail.com>
2023-04-21 16:16:49 +1200

    reduced repetition

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -4,206 +4,206 @@ import java.util.LinkedHashMap;
 import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
-  private Integer numberOfProfiles;
-  // using LinkedHashMap to preserve the order of insertion
-  private LinkedHashMap<String, Object> users;
-  private User loadedUser;
+	private Integer numberOfProfiles;
+	// using LinkedHashMap to preserve the order of insertion
+	private LinkedHashMap<String, Object> users;
+	private User loadedUser;
 
-  public InsuranceSystem() {
-    // Only this constructor can be used (if you need to initialise fields).
-    this.numberOfProfiles = 0;
-    this.users = new LinkedHashMap<String, Object>();
+	public InsuranceSystem() {
+		// Only this constructor can be used (if you need to initialise fields).
+		this.numberOfProfiles = 0;
+		this.users = new LinkedHashMap<String, Object>();
 
-    this.loadedUser = null;
-  }
+		this.loadedUser = null;
+	}
 
-  public void printDatabase() {
-    // Prints the right message depending on the number of profiles.
-    if (this.numberOfProfiles == 0) {
-      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(this.numberOfProfiles.toString(), "s", ".");
-      return;
-    } else if (this.numberOfProfiles == 1) {
-      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(this.numberOfProfiles.toString(), "", ":");
-    } else {
-      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(this.numberOfProfiles.toString(), "s", ":");
-    }
+	public void printDatabase() {
+		// Prints the right message depending on the number of profiles.
+		if (this.numberOfProfiles == 0) {
+			MessageCli.PRINT_DB_POLICY_COUNT.printMessage(this.numberOfProfiles.toString(), "s", ".");
+			return;
+		} else if (this.numberOfProfiles == 1) {
+			MessageCli.PRINT_DB_POLICY_COUNT.printMessage(this.numberOfProfiles.toString(), "", ":");
+		} else {
+			MessageCli.PRINT_DB_POLICY_COUNT.printMessage(this.numberOfProfiles.toString(), "s", ":");
+		}
 
-    // iterates through the users stored in the hash
-    Integer i = 0;
+		// iterates through the users stored in the hash
+		Integer i = 0;
 
-    for (String userName : users.keySet()) {
-      i++;
-      // gets the user object from the hash using the username
-      User user = (User) users.get(userName);
-      // prints the details from user object
-      // if the user is the loaded user, print asterisks
+		for (String userName : users.keySet()) {
+			i++;
+			// gets the user object from the hash using the username
+			User user = (User) users.get(userName);
 
-      String active = "";
+			// prints the details from user object
+			String active = "";
 
-      // if the user is the loaded user, print asterisks
-      if (this.loadedUser == user)
-        active = "*** ";
+			// if the user is the loaded user, print asterisks
+			if (this.loadedUser == user)
+				active = "*** ";
 
-      // check details and print the number of policies
-      if (user.getNumberOfPolicies() == 0) {
-        MessageCli.PRINT_DB_PROFILE_HEADER_LONG.printMessage(active, i.toString(), userName,
-            Integer.toString(user.getAge()), Integer.toString(user.getNumberOfPolicies()), "ies",
-            Integer.toString((int) user.getTotalPremium()));
-      } else if (user.getNumberOfPolicies() == 1) {
-        MessageCli.PRINT_DB_PROFILE_HEADER_LONG.printMessage(active, i.toString(), userName,
-            Integer.toString(user.getAge()), Integer.toString(user.getNumberOfPolicies()), "y",
-            Integer.toString((int) user.getTotalPremium()));
-      } else {
-        MessageCli.PRINT_DB_PROFILE_HEADER_LONG.printMessage(active, i.toString(), userName,
-            Integer.toString(user.getAge()), Integer.toString(user.getNumberOfPolicies()), "ies",
-            Integer.toString((int) user.getTotalPremium()));
-      }
-      user.printPolicies();
-    }
+			// check details and print the number of policies
+			String plurality;
 
-  }
+			if (user.getNumberOfPolicies() == 1) {
+				plurality = "y";
+			} else {
+				plurality = "ies";
+			}
 
-  private int parseAge(String age) {
-    // Parses the age string into an integer.
-    // If the age is invalid, returns -1.
-    try {
-      return Integer.parseInt(age);
-    } catch (NumberFormatException e) {
-      return -1;
-    }
-  }
+			MessageCli.PRINT_DB_PROFILE_HEADER_LONG.printMessage(active, i.toString(), userName,
+					Integer.toString(user.getAge()), Integer.toString(user.getNumberOfPolicies()), plurality,
+					Integer.toString((int) user.getTotalPremium()));
 
-  public void createNewProfile(String userName, String age) {
+			// prints the policies
+			user.printPolicies();
+		}
 
-    int ageInt = parseAge(age);
+	}
 
-    String formattedUserName = makeTitleCase(userName);
+	private int parseAge(String age) {
+		// Parses the age string into an integer.
+		// If the age is invalid, returns -1.
+		try {
+			return Integer.parseInt(age);
+		} catch (NumberFormatException e) {
+			return -1;
+		}
+	}
 
-    // validation
-    if (ageInt == -1) {
-      // checking if age is an integer
-      MessageCli.INVALID_AGE.printMessage(age, formattedUserName);
-      return;
-    } else if (this.loadedUser != null) {
-      // see if a user is already loaded
-      MessageCli.CANNOT_CREATE_WHILE_LOADED.printMessage(loadedUser.getUserName());
-      return;
-    } else if (formattedUserName.length() < 3) {
-      // see if the username is too short
-      MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(formattedUserName);
-      return;
-    } else if (ageInt < 1 || ageInt > 120) {
-      // see if the age is invalid
-      MessageCli.INVALID_AGE.printMessage(age, formattedUserName);
-      return;
-    } else if (users.get(formattedUserName) != null) {
-      // see if the username is already taken
-      MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(formattedUserName);
-      return;
-    }
+	public void createNewProfile(String userName, String age) {
 
-    // creating new user and adding to hash
-    User user = new User(formattedUserName, ageInt);
-    users.put(formattedUserName, user);
-    this.numberOfProfiles++;
-    // prints integer to string to prevent user formatting while printing
-    MessageCli.PROFILE_CREATED.printMessage(formattedUserName, Integer.toString(ageInt));
+		int ageInt = parseAge(age);
 
-  }
+		String formattedUserName = makeTitleCase(userName);
 
-  private String makeTitleCase(String input) {
-    // for formatting userName
-    String formattedString = input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
-    return formattedString;
-  }
+		// validation
+		if (ageInt == -1) {
+			// checking if age is an integer
+			MessageCli.INVALID_AGE.printMessage(age, formattedUserName);
+			return;
+		} else if (this.loadedUser != null) {
+			// see if a user is already loaded
+			MessageCli.CANNOT_CREATE_WHILE_LOADED.printMessage(loadedUser.getUserName());
+			return;
+		} else if (formattedUserName.length() < 3) {
+			// see if the username is too short
+			MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(formattedUserName);
+			return;
+		} else if (ageInt < 1 || ageInt > 120) {
+			// see if the age is invalid
+			MessageCli.INVALID_AGE.printMessage(age, formattedUserName);
+			return;
+		} else if (users.get(formattedUserName) != null) {
+			// see if the username is already taken
+			MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(formattedUserName);
+			return;
+		}
 
-  private Boolean parseToBoolean(String input) {
-    // for parsing boolean input
-    if (input.toLowerCase().startsWith("y"))
-      return true;
-    return false;
-  }
+		// creating new user and adding to hash
+		User user = new User(formattedUserName, ageInt);
+		users.put(formattedUserName, user);
+		this.numberOfProfiles++;
+		// prints integer to string to prevent user formatting while printing
+		MessageCli.PROFILE_CREATED.printMessage(formattedUserName, Integer.toString(ageInt));
 
-  public void loadProfile(String userName) {
-    String formattedUserName = makeTitleCase(userName);
+	}
 
-    // setting the loaded user to be the inputed user
-    User userToBeLoaded = (User) users.get(formattedUserName);
+	private String makeTitleCase(String input) {
+		// for formatting userName
+		String formattedString = input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
+		return formattedString;
+	}
 
-    // check if user exists
-    if (userToBeLoaded == null) {
-      MessageCli.NO_PROFILE_FOUND_TO_LOAD.printMessage(formattedUserName);
-      return;
-    }
+	private Boolean parseToBoolean(String input) {
+		// for parsing boolean input
+		if (input.toLowerCase().startsWith("y"))
+			return true;
+		return false;
+	}
 
-    this.loadedUser = userToBeLoaded;
+	public void loadProfile(String userName) {
+		String formattedUserName = makeTitleCase(userName);
 
-    // successfully loaded
-    MessageCli.PROFILE_LOADED.printMessage(formattedUserName);
-  }
+		// setting the loaded user to be the inputed user
+		User userToBeLoaded = (User) users.get(formattedUserName);
 
-  public void unloadProfile() {
-    // check if a user is loaded
-    if (this.loadedUser == null) {
-      MessageCli.NO_PROFILE_LOADED.printMessage();
-      return;
-    }
+		// check if user exists
+		if (userToBeLoaded == null) {
+			MessageCli.NO_PROFILE_FOUND_TO_LOAD.printMessage(formattedUserName);
+			return;
+		}
 
-    // unloading the user
-    String userName = this.loadedUser.getUserName();
-    this.loadedUser = null;
-    MessageCli.PROFILE_UNLOADED.printMessage(userName);
-  }
+		this.loadedUser = userToBeLoaded;
 
-  public void deleteProfile(String userName) {
-    // check if user exists
-    String formattedUserName = makeTitleCase(userName);
-    if (users.get(formattedUserName) == null) {
-      MessageCli.NO_PROFILE_FOUND_TO_DELETE.printMessage(formattedUserName);
-      return;
-    }
+		// successfully loaded
+		MessageCli.PROFILE_LOADED.printMessage(formattedUserName);
+	}
 
-    // check if user is loaded, return error if so
-    if (this.loadedUser == users.get(formattedUserName)) {
-      MessageCli.CANNOT_DELETE_PROFILE_WHILE_LOADED.printMessage(formattedUserName);
-      return;
-    }
+	public void unloadProfile() {
+		// check if a user is loaded
+		if (this.loadedUser == null) {
+			MessageCli.NO_PROFILE_LOADED.printMessage();
+			return;
+		}
 
-    // delete the user
-    users.remove(formattedUserName);
-    this.numberOfProfiles--;
-    MessageCli.PROFILE_DELETED.printMessage(formattedUserName);
-  }
+		// unloading the user
+		String userName = this.loadedUser.getUserName();
+		this.loadedUser = null;
+		MessageCli.PROFILE_UNLOADED.printMessage(userName);
+	}
 
-  public void createPolicy(PolicyType type, String[] options) {
-    // check if a user is loaded
-    if (this.loadedUser == null) {
-      MessageCli.NO_PROFILE_FOUND_TO_CREATE_POLICY.printMessage();
-      return;
-    }
+	public void deleteProfile(String userName) {
+		// check if user exists
+		String formattedUserName = makeTitleCase(userName);
+		if (users.get(formattedUserName) == null) {
+			MessageCli.NO_PROFILE_FOUND_TO_DELETE.printMessage(formattedUserName);
+			return;
+		}
 
-    int sumInsured = Integer.parseInt(options[0]);
+		// check if user is loaded, return error if so
+		if (this.loadedUser == users.get(formattedUserName)) {
+			MessageCli.CANNOT_DELETE_PROFILE_WHILE_LOADED.printMessage(formattedUserName);
+			return;
+		}
 
-    InsurancePolicy policyToAdd;
+		// delete the user
+		users.remove(formattedUserName);
+		this.numberOfProfiles--;
+		MessageCli.PROFILE_DELETED.printMessage(formattedUserName);
+	}
 
-    switch (type) {
-      case CAR:
-        policyToAdd = new PolicyCar(loadedUser, sumInsured, options[1], options[2],
-            this.parseToBoolean(options[3]));
-        break;
-      case HOME:
-        policyToAdd = new PolicyHome(loadedUser, sumInsured, options[1], this.parseToBoolean(options[2]));
-        break;
-      case LIFE:
-        policyToAdd = new PolicyLife(loadedUser, sumInsured);
-        break;
-      default:
-        policyToAdd = null;
-        break;
-    }
+	public void createPolicy(PolicyType type, String[] options) {
+		// check if a user is loaded
+		if (this.loadedUser == null) {
+			MessageCli.NO_PROFILE_FOUND_TO_CREATE_POLICY.printMessage();
+			return;
+		}
 
-    // add policy to user
-    loadedUser.addPolicy(policyToAdd);
+		int sumInsured = Integer.parseInt(options[0]);
 
-  }
+		// define policy
+		InsurancePolicy policyToAdd;
+
+		switch (type) {
+			case CAR:
+				policyToAdd = new PolicyCar(loadedUser, sumInsured, options[1], options[2],
+						this.parseToBoolean(options[3]));
+				break;
+			case HOME:
+				policyToAdd = new PolicyHome(loadedUser, sumInsured, options[1], this.parseToBoolean(options[2]));
+				break;
+			case LIFE:
+				policyToAdd = new PolicyLife(loadedUser, sumInsured);
+				break;
+			default:
+				policyToAdd = null;
+				break;
+		}
+
+		// add policy to user
+		loadedUser.addPolicy(policyToAdd);
+
+	}
 }
